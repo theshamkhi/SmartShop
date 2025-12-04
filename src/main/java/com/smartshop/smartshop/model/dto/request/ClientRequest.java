89a1ebfaa1175d589e740dd82b25e252339e7d0d
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-12-04 19:18:25 +0100

    feat: add ClientRequest DTO for client creation

---
+++ src/main/java/com/smartshop/smartshop/model/dto/request/ClientRequest.java
@@ -0,0 +1,27 @@
+package com.smartshop.smartshop.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClientRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Name is required")
+    private String nom;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+}
