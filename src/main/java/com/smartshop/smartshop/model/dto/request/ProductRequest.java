85afccb62d7a5a6908cec767c99887b70187cbb9
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-12-04 19:18:26 +0100

    feat: add ProductRequest DTO with price validation

---
+++ src/main/java/com/smartshop/smartshop/model/dto/request/ProductRequest.java
@@ -0,0 +1,29 @@
+package com.smartshop.smartshop.model.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductRequest {
+    @NotBlank(message = "Product name is required")
+    private String nom;
+
+    @NotNull(message = "Unit price is required")
+    @DecimalMin(value = "0.01", message = "Price must be greater than 0")
+    private BigDecimal prixUnitaire;
+
+    @NotNull(message = "Stock is required")
+    @Min(value = 0, message = "Stock cannot be negative")
+    private Integer stockDisponible;
+}
